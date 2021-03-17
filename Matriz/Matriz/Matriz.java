84d8dc29428e2e42f16644ad3d76bdbd36e18893
Abel2299 <74042613+Abel2299@users.noreply.github.com>
2021-03-17 09:08:05 +0100

    Matriz
    Matriz

--- Matriz/Matriz/Matriz.java
+++ Matriz/Matriz/Matriz.java
@@ -58,6 +58,18 @@ public class Matriz
         else{
             throw new IllegalArgumentException("Columna incorrecta");
         }
+        
+        /*
+         * int[] col=null;
+         * if(columnas<0 || columna>=columnas){
+         *   throw new IllegalArgumentException("Fila o columna incorrecto");
+        }
+        
+        col=new int[filas]
+       for(int i=0;i<filas;i++){
+                arrayColumna [i]=elementos[i][columna];
+            }
+         */
     }  
     
     public float [] calcularMediaColumnas(){
@@ -82,34 +94,53 @@ public class Matriz
     
     public boolean existe(int n){
         boolean existe=false;
- 
         for(int i=0;i<filas && !existe;i++){
             for(int j=0;j<columnas && !existe;j++){
-		if(elementos[i][j]==n){
-		    existe=true;
-		}
-	    }
-	}
+                if(elementos[i][j]==n){
+                    existe=true;
+                }
+            }   
+        }
         
         return existe;
     }
     
     public void intercambiarColumnas(int columna1, int columna2){
+        if(columna1<0 || columna1>=columnas || columna2<0 || columna2>=columnas){
+            throw new IllegalArgumentException("Columna incorrecta");
+        }
         int [] colum1, colum2;        
         colum1=getColumna(columna1);
         colum2=getColumna(columna2);
-        for(int i=0;i<filas;i++){
-            elementos[i][columna1]=colum2[i];
-            elementos[i][columna2]=colum1[i];
+        if(colum1!=colum2){
+            for(int i=0;i<filas;i++){
+                elementos[i][columna1]=colum2[i];
+                elementos[i][columna2]=colum1[i];
+            }
         }
     }
     
-    /*public Matriz borrarColumna(int columna){
-        
-    }*/
+    public Matriz borrarColumna(int columna){
+        Matriz m=null;
+        if(columna<0 || columna>=columnas){
+            throw new IllegalArgumentException("Error en la columna");
+        }
+        if(columnas>1){
+            m=new Matriz(filas,columnas);
+                for(int j=0,k=0;j<columnas;j++){
+                    if(j!=columna){
+                    for(int i=0;i<filas;i++){
+                        m.elementos[i][k]=elementos[i][j];
+                    }
+                    k++;
+                }
+            }
+        }   
+        return m;
+    }
     
     public Matriz sumar(Matriz otraMatriz){
-        int fila1=0,columna1=0;
+        /*int fila1=0,columna1=0;
         int filas=0,columnas=0;
         int filaN,columnaN;
         this.filas=filas;
@@ -121,16 +152,60 @@ public class Matriz
             }
         }
         return otraMatriz;
+        */
+        Matriz m=null;
+        if(otraMatriz!=null){
+            if(otraMatriz.filas!=filas ||otraMatriz.columnas!=columnas){
+                throw new IllegalArgumentException("Matriz no valida");
+            }
+            m=new Matriz(filas,columnas);
+            for(int i=0;i<filas;i++){
+            for(int j=0;j<columnas;j++){
+                m.elementos[i][j]=elementos[i][j]+otraMatriz.elementos[i][j];       
+            }   
+        }
+        }
+        
+        
+        return m;
     }
     
-    public Matriz trasponer(){
-       Matriz trasponer ;
-       trasponer=new Matriz(filas,columnas);
+    public Matriz transponer(){
+       Matriz transponer ;
+       transponer=new Matriz(filas,columnas);
        for(int i=0;i<filas;i++){
-           for(int j=0;j<columnas[i];j++){
-		
-            }	
-	}
-	return trasponer;
+           for(int j=0;j<columnas;j++){
+               transponer.elementos[i][j]=elementos[i][j];
+            }   
+    }
+    return transponer;
+    }
+    
+    /*public boolean equals(Object obj){
+        
+    }*/
+    
+    public static boolean esSimetrica(Matriz matriz){
+        boolean simetrica=false;
+        
+        if(matriz!=null){
+            if(matriz.filas==matriz.columnas && matriz.equals(matriz.transponer())){
+                simetrica=true;
+            }
+        }
+        
+        return simetrica;
+    }
+    
+    public String toString(){
+        StringBuilder sb=new StringBuilder();
+        
+        for(int[] fila:elementos){
+            for(int elemento: fila){
+                sb.append(elemento);
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
 }
